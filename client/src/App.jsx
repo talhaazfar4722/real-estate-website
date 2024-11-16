5da9cab405ba4d71a70c301c182e4dd00bd88542
Talha Azfar <113213168+talhaazfar4722@users.noreply.github.com>
2024-11-16 20:22:43 +0500

    install react router dom and set pages and route

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,22 @@
 import React from 'react'
-
+import {BrowserRouter, Routes, Route} from 'react-router-dom'
+import Home from './pages/Home'
+import SignIn from './pages/SignIn'
+import SignUp from './pages/SignUp'
+import About from './pages/About'
+import Profile from './pages/Profile'
 export default function App() {
   return (
-    <div>
-      app
+    <div className='px-4 sm:px-[5vw] md:px-[7vw] lg:px-[9vw]'>
+      <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Home/>}/>
+        <Route path='/sign-in' element={<SignIn/>}/>
+        <Route path='/sign-up' element={<SignUp/>}/>
+        <Route path='/about' element={<About/>}/>
+        <Route path='/profile' element={<Profile/>}/>
+      </Routes>
+      </BrowserRouter>
     </div>
   )
 }
