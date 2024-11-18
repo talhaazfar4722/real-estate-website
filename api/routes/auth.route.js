93f31ee7685bd4483f8bc17754a8a5b201b3484d
Talha Azfar <113213168+talhaazfar4722@users.noreply.github.com>
2024-11-18 23:38:35 +0500

    created signin api route

--- api/routes/auth.route.js
+++ api/routes/auth.route.js
@@ -1,10 +1,12 @@
 import express from 'express';
-import { signup } from '../controllers/auth.controller.js';
+import { signin, signup } from '../controllers/auth.controller.js';
 
 
 const router = express.Router();
 
 // signup api route
 router.post("/signup", signup);
+// sigin api route
+router.post("/signin", signin);
 
 export default router;
