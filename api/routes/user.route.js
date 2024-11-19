4c3d1c9cd0237abecafee737dac9e3558ad15989
Talha Azfar <113213168+talhaazfar4722@users.noreply.github.com>
2024-11-19 18:11:33 +0500

    create update user api route

--- api/routes/user.route.js
+++ api/routes/user.route.js
@@ -1,9 +1,12 @@
 import express from 'express';
-import { test } from '../controllers/user.controller.js';
+import { test, updateUser } from '../controllers/user.controller.js';
+import { verifyToken } from '../utils/verifyUser.js';
 
 const router = express.Router();
 
 // test api route
 router.get('/test', test);
+// update user api route
+router.post('/update/:id', verifyToken, updateUser)
 
 export default router;
