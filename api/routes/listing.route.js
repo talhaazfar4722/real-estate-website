5b01ff9555963a7b5972b789ca71833ac3a58e0d
Talha Azfar <113213168+talhaazfar4722@users.noreply.github.com>
2024-11-21 13:07:44 +0500

    created update listing api route

--- api/routes/listing.route.js
+++ api/routes/listing.route.js
@@ -1,7 +1,7 @@
 
 
 import express from 'express';
-import { createListing, deleteListing, } from '../controllers/listing.controller.js';
+import { createListing, deleteListing, updateListing, } from '../controllers/listing.controller.js';
 import { verifyToken } from '../utils/verifyUser.js';
 
 const router = express.Router();
@@ -9,5 +9,7 @@ const router = express.Router();
 router.post('/create', verifyToken, createListing);
 // delete listing route
 router.delete('/delete/:id', verifyToken, deleteListing);
+// update listing route
+router.post('/update/:id', verifyToken, updateListing);
 
 export default router;
--- client/src/components/UserListings.jsx
+++ client/src/components/UserListings.jsx
@@ -58,7 +58,7 @@ export default function UserListings() {
       </p>
       {userListings &&
         userListings.length > 0 &&
-        <div className="flex flex-col gap-4 bg-white p-1 sm:p-5 w-full">
+        <div className="flex flex-col gap-4 bg-white p-1 sm:p-5 w-full rounded-lg">
           <h1 className='text-center text-2xl font-semibold text-black'>Your Listings</h1>
           {userListings.map((listing) => (
             <div
@@ -79,9 +79,9 @@ export default function UserListings() {
                 <p>{listing.name}</p>
               </Link>
   
-              <div className='flex gap-5   '>
-                <button className='text-red-700 uppercase'  onClick={() => handleListingDelete(listing._id)}>Delete</button>
-                <button className='text-green-700 uppercase'>Edit</button>
+              <div className='flex gap-2 sm:gap-5   '>
+                <button className='text-red-700 uppercase text-sm sm:text-lg'  onClick={() => handleListingDelete(listing._id)}>Delete</button>
+                <button className='text-green-700 uppercase text-sm sm:text-lg'>Edit</button>
               </div>
             </div>
           ))}
