5ecf319176834349498fa70c926778e3554913f5
Talha Azfar <113213168+talhaazfar4722@users.noreply.github.com>
2024-11-22 15:16:36 +0500

    Create search page UI

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -11,6 +11,7 @@ import PrivateRoute from './components/PrivateRoute';
 import Navbar2 from './components/Navbar2';
 import UpdateListing from './pages/UpdateListing';
 import Listing from './pages/Listing';
+import Search from './pages/Search';
 
 
 
@@ -30,6 +31,7 @@ function App() {
         <Route path='/sign-in' element={<SignIn />} />
         <Route path='/sign-up' element={<SignUp />} />
         <Route path='/about' element={<About />} />
+        <Route path='/search' element={<Search />} />
         <Route path='/listing/:listingId' element={<Listing />} />
         <Route element={<PrivateRoute />}>
         <Route path='/profile' element={<Profile />} />
